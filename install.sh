355a1276e8d9a4985f7b17b7dc44bec840bebfd2
skurrrrr <wirelessdan@proton.me>
2025-01-22 16:27:02 -0800

    took out hardcoded webhook for security reasons

--- install.sh
+++ install.sh
@@ -5,11 +5,13 @@ chmod +x /usr/local/bin/quasar/check_ip.py /usr/local/bin/quasar/reboot.py
 
 read -p "What shall we call this machine?: " this_machine
 
+read -p "Webhook for this machine (should not be exposed to the public): " webhook
+
 # Get public IP address using curl command
 public_ip=$(curl -4 http://icanhazip.com)
 
 # Create the quasar_ config.json file in /usr/local/bin/quasar
-echo "{\"this machine\": \"$this_machine\", \"API\": \"milfrogs.com\", \"IP\": \"$public_ip\"}" > /usr/local/bin/quasar/quasar_config.json
+echo "{\"this machine\": \"$this_machine\", \"webhook\": \"$webhook\", \"API\": \"milfrogs.com\", \"IP\": \"$public_ip\"}" > /usr/local/bin/quasar/quasar_config.json
 
 # Give the file read permission to everyone
 chmod 755 /usr/local/bin/quasar/quasar_config.json
